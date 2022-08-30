27e366c4f6a518e12f2361f32e1ab2b6af0006e9
Stephen Ng <kmng@ucla.edu>
2022-08-30 21:00:30 +0000

    add iam for connection to session manager

--- modules/terraform-aws-ec2/autoscaling_group.tf
+++ modules/terraform-aws-ec2/autoscaling_group.tf
@@ -6,6 +6,12 @@ resource "aws_launch_template" "web-server" {
 
   user_data = var.user_data
 
+
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.session-manager-instance-profile.name
+  }
+
   network_interfaces {
     associate_public_ip_address = false
     security_groups             = [aws_security_group.web.id]
@@ -35,5 +41,96 @@ resource "aws_autoscaling_group" "web-servers" {
   launch_template {
     id = aws_launch_template.web-server.id
 
+  }
+}
+
+resource "aws_iam_role" "session-manager-role" {
+  name = "session-manager-role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+
+  tags = {
+    Terraform = "true"
+    Name      = "session-manager-role-${var.stack_name}"
+  }
+}
+
+resource "aws_iam_role_policy" "session-manager-policy" {
+  name = "session-manager-policy"
+  role = aws_iam_role.session-manager-role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_instance_profile" "session-manager-instance-profile" {
+  name = "session-manager"
+  role = aws_iam_role.session-manager-role.name
+
+  tags = {
+    Terraform = "true"
+    Name      = "session-manager-instance-profile-${var.stack_name}"
   }
 }
